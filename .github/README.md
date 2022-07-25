d44be51dd2fcff7c0684f2f6228d74b417a4498d
Francis Masha <francismasha96@gmail.com>
2022-07-25 22:39:38 +0300

    feat: implement logging

--- .github/README.md
+++ .github/README.md
@@ -1,7 +1,6 @@
-# code-rust
+# newsletter rust app
 
 ![github actions](https://github.com/mashafrancis/code-rust/workflows/build/badge.svg)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/6c3b8b1f11f67ee7dcf8/test_coverage)](https://codeclimate.com/github/mashafrancis/newsletter/test_coverage)
 
 
-rust algorithms and data structures
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use newsletter::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+	let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
+	init_subscriber(subscriber);
+
 	// Panic if we can't read configuration
 	let configuration = get_configuration().expect("Failed to read configuration.");
-	let connection_pool = PgPool::connect(&configuration.database.connection_string())
-		.await
-		.expect("Failed to connect to Postgres.");
-	// We have removed the hard-coded `8000` - it's now coming from our settings!
+	let connection_pool =
+		PgPool::connect(&configuration.database.connection_string().expose_secret())
+			.await
+			.expect("Failed to connect to Postgres.");
+
 	let address = format!("127.0.0.1:{}", configuration.application_port);
 	let listener = TcpListener::bind(address)?;
-	run(listener, connection_pool)?.await
+	run(listener, connection_pool)?.await?;
+	Ok(())
 }
