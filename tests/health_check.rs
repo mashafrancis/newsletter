073c59559e1e35af84a14d94ac07ef70d71d2e6a
Francis Masha <francismasha96@gmail.com>
2022-07-30 09:10:34 +0300

    feat: rejecting invalid subscribers

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -147,7 +147,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
 	// Arrange
 	let app = spawn_app().await;
 	let client = reqwest::Client::new();
@@ -169,9 +169,10 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
 
 		// Assert
 		assert_eq!(
-			200,
+			// Not 200 anymore!
+			400,
 			response.status().as_u16(),
-			"The API did not return a 200 OK when the payload was {}.",
+			"The API did not return a 400 Bad Request when the payload was {}.",
 			description
 		);
 	}
